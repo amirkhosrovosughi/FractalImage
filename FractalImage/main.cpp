a8b2bd64c7c59d5e12645904ea1bcb8d633d4e62
Amir <amirkhosro.vosughi@gmail.com>
2019-09-23 16:06:38 -0700

    Add headers and Main  class

--- FractalImage/main.cpp
+++ FractalImage/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+#include "C:\Users\Amir\Desktop\Gtt\FractalImage\FractalImage\include\Bitmap.h"
+
 
 using namespace std;
+using namespace caveofprogramming;
 
 int main()
 {
+    //Bitmap mtmp1(800,800);
+    Bitmap btmp1(700,700);
+    btmp1.write("test1.bmp");
     cout << "Hello world!" << endl;
     return 0;
 }
---
+++ FractalImage/src/Bitmap.cpp
@@ -0,0 +1,53 @@
+#include "Bitmap.h"
+#include <fstream>
+//#include <memory>
+#include "C:\Users\Amir\Desktop\Gtt\FractalImage\FractalImage\BitmapFileHeader.h"
+#include "C:\Users\Amir\Desktop\Gtt\FractalImage\FractalImage\BitmapInfoHeader.h"
+
+using namespace caveofprogramming;
+using namespace std;
+
+caveofprogramming::Bitmap::Bitmap(int width, int height): m_width(width), m_height(height), m_pPixel(new uint8_t[3*width*height]{})
+{
+    //ctor
+}
+
+bool caveofprogramming::Bitmap::write(string filename){
+
+    BitmapFileHeader fileHeader;
+    BitmapInfoHeader infoHeader;
+
+    fileHeader.fileSize = sizeof(BitmapFileHeader)+sizeof(BitmapInfoHeader)+3*m_width*m_height;
+    fileHeader.dataOffset = sizeof(BitmapFileHeader)+sizeof(BitmapInfoHeader);
+
+    infoHeader.width = m_width;
+    infoHeader.height = m_height;
+
+
+    ofstream file;
+    file.open(filename, ios::out|ios::binary);
+
+    if(!file){
+        return false;
+    }
+
+    file.write((char *)&fileHeader, sizeof(fileHeader));
+    file.write((char *)&infoHeader, sizeof(infoHeader));
+    file.write((char *)m_pPixel.get(), m_width*m_height*3);
+
+    file.close();
+    if(!file){
+        return false;
+    }
+
+    return true;
+}
+
+
+void caveofprogramming::Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){
+}
+
+caveofprogramming::Bitmap::~Bitmap()
+{
+    //dtor
+}
